df5a05a1c10776844bc42fc226d51677b106e775
Sara Regan <regan.sara@gmail.com>
2019-02-17 22:33:00 +0100

    Add notes from section 'Mutability of Objects'

--- 1-1-expressive-javascript.js
+++ 1-1-expressive-javascript.js
@@ -31,7 +31,7 @@ myAnimation.stop()
 
 // If you prefer doing it all in one declaration
 
-//Animation class with a slightly different syntax for declaration methods
+//Animation class with a slightly different syntax for declaring methods
 
 var Animation = function () {
  // ...
@@ -48,6 +48,7 @@ Animation.prototype = {
 
 
 // Add a method to the Function object that can be use to declare methods
+
 Function.prototype.method = function (name, fn) {
   this.prototype[name] = fn
 } 
@@ -107,7 +108,7 @@ Animation
   alert(foo * bar)
 })(10, 2)
 
-// Instead of assignging the foo and bar inside the function, they are passed in as parameters.
+// Instead of assigning `foo` and `bar` inside the function, they are passed in as parameters.
 
 // You can also return a value from a function; below is an example:
 var baz = (function (foo, bar) {
@@ -115,10 +116,10 @@ var baz = (function (foo, bar) {
 })(10, 2)
 // baz will equal 20
 
-// #### Function as a first class objects
-// The most interesting use of anonymous functions is to create a closure; a protected variable space, created by using nesting function;
-// In JavaScript, a variable defined within a function is not accessible outside of it; this is called function-level scope. 
-// In addition, functions run in the scope they are defined in, not in the scope they are executed in; this is called lexical-scope.
+// #### Functions as first class objects
+// The most interesting use of anonymous functions is to create a closure; a protected variable space, created by nesting functions.
+// In JavaScript, a variable defined within a function is not accessible outside of it; function-level scope. 
+// In addition, functions run in the scope they are defined in, not in the scope they are executed in; lexical-scope.
 // These aspects of JavaScript can allow you to create private variables - by wrapping them in anonymous functions.
 
 // An anonymous function used as a closure
@@ -136,4 +137,52 @@ baz()
 // baz can access foo and bar, even though it is executed outside of the anonymous function
 
 
-// Stopping point The Mutability of Objects
+// #### The Mutability of Objects
+
+// In JavaScript everything is an object and everything is mutable
+//This means you can give attributes to functions:
+
+function displayError (message) {
+  displayError.numTimesExecuted++
+  alert(message)
+}
+displayError.numTimesExecuted = 0
+
+//It also means you can modify classes and objects after they have been created;
+//Class Person
+function Person (name, age) {
+  this.name = name
+  this.age = age
+}
+
+Person.prototype = {
+  getName: function () {
+    return this.name
+  },
+  getAge: function () {
+    return this.age
+  }
+}
+
+// Instantiate the classs
+var alice = new Person('Alice', 93)
+var bill = new Person('Bill', 30)
+
+// Modify the Class
+Person.prototype.getGreeting = function () {
+  return 'Hi' + this.getName() + '!'
+}
+
+// Modify a specific instance
+alice.displayGreeting = function () {
+  alert(this.getGreeting())
+}
+
+// Both instances of Person get the `getGreeting` method, due to how the prototype object work
+// However, only the `alice` instance gets the `displayGreeting` method; no other instance does.
+
+// Introspection and reflection
+// introspection is the ability to examine any object at run time to see what attributes and methods it contains.
+// 'Reflection' is using this information to instantiate classes and execute methods dynamically without knowing
+// their names at development time
+// In JavaScript everything can be modified at run-time
